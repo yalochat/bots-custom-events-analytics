8c95dc0808c43f499a70bcf560693c763d72df18
Fred Allen <edwin.allen@gmail.com>
2017-01-19 11:33:49 -0600

    [Add] suport to events with query params
    	modified:   lib/index.js
    	modified:   test/index.js

--- test/index.js
+++ test/index.js
@@ -23,7 +23,15 @@ describe('track analytic', () => {
         Analytics.init('fredtest', 1746825388870241, 345741605489510)
         Analytics.track('993939393','goto_dfdf-dfdf-ddd').then(res => {
 
-            expect(res.body).to.equals('')
+            expect(res.body.success).to.be.equals('')
+        }).catch(done)
+    })
+
+    it('track en event with query params', (done) => {
+        Analytics.init('fredtest', 1746825388870241, 345741605489510)
+        Analytics.track('993939393','goto_dfdf-dfdf-ddd?hello-world').then(res => {
+
+            expect(res.body.success).to.be.equals('')
         }).catch(done)
     })
 })
